0d46d2139cc908c291ae63f9b2241298a4faa340
dlwhitehurst <dlwhitehurst@me.com>
2016-01-15 19:57:56 -0500

    initial check-in

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name ciwiseGenledgerApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the ciwiseGenledgerApp
+ */
+angular.module('ciwiseGenledgerApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name ciwiseGenledgerApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the ciwiseGenledgerApp
+ */
+angular.module('ciwiseGenledgerApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
