3201ac893b385fc888e3b7d07bb939e6cc71ffc7
dlwhitehurst <dlwhitehurst@me.com>
2016-01-16 01:57:42 -0500

    roughed out home page

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -29,7 +29,15 @@ angular
         controller: 'AboutCtrl',
         controllerAs: 'about'
       })
+      .when('/contact', {
+        templateUrl: 'views/contact.html',
+        controller: 'ContactCtrl',
+        controllerAs: 'contact'
+      })
       .otherwise({
         redirectTo: '/'
       });
   });
+
+  $.material.init();
+
---
+++ app/scripts/controllers/contact.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name ciwiseGenledgerApp.controller:ContactCtrl
+ * @description
+ * # ContactCtrl
+ * Controller of the ciwiseGenledgerApp
+ */
+angular.module('ciwiseGenledgerApp')
+  .controller('ContactCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
